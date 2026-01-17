1aa93e470341d3111114f40188095a41a199a729
helgituan <helgih20@ru.is>
2026-01-17 23:04:02 +0000

    Started to do the cards

--- components/landing-page/featured-products.tsx
+++ components/landing-page/featured-products.tsx
@@ -2,6 +2,42 @@ import SectionHeader from "@/components/common/section-header";
 import { ArrowUpRightIcon, StarIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ProductCard from "@/components/products/product-card";
+
+const featuredProducts = [
+  {
+    id: 1,
+    name: "ParityKit",
+    description: "A toolkit for creating parity products",
+    tags: ["Saas", "Pricing", "Global"],
+    votes: 615,
+    isFeatured: true,
+  },
+  {
+    id: 2,
+    name: "Modern Full Stack Next.js Course",
+    description: "Learn to build modern fullstack application with Next.js",
+    tags: ["Next.js", "Tailwind CSS", "Full Stack"],
+    votes: 124,
+    isFeatured: true,
+  },
+  {
+    id: 3,
+    name: "Developer to Leader",
+    description: "A course on Engineering Leadership",
+    tags: ["Course, Leadership"],
+    votes: 503,
+    isFeatured: true,
+  },
+  {
+    id: 4,
+    name: "ProofyBubble",
+    description: "Social proof notifications that convert visitors",
+    tags: ["Marketing", "SaaS", "Conversion"],
+    votes: 531,
+    isFeatured: true,
+  },
+];
 
 export default function FeaturedProducts() {
   return (
@@ -19,6 +55,11 @@ export default function FeaturedProducts() {
               <ArrowUpRightIcon className="size-4" />
             </Link>
           </Button>
+        </div>
+        <div className="grid-wrapper">
+          {featuredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       </div>
     </section>
